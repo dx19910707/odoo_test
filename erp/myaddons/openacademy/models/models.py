7b01ed8d1495f21e4d5e9df82fab6691323d50fc
dx19910707 <dx19910707@qq.com>
2019-03-08 17:03:10 +0800

    Computed fields and default values

--- erp/myaddons/openacademy/models/models.py
+++ erp/myaddons/openacademy/models/models.py
@@ -17,9 +17,19 @@ class Session(models.Model):
     _description = 'session'
 
     name = fields.Char(string='session', required=True)
-    start_date = fields.Date(string='开始日期')
+    start_date = fields.Date(string='开始日期', default=fields.Date.today)
     duration = fields.Float(digits=(6,2), help="持续时间", string='持续时间')
     seats = fields.Integer(string='座位数')
+    active = fields.Boolean(default=True)
     instructor_id = fields.Many2one('res.partner', string="指导员")
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="课程", required=True)
     attendee_ids = fields.Many2many('res.partner', string="参加人员")
+    taken_seats = fields.Float(string='位置占用百分比', compute='_compute_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _compute_taken_seats(self):
+        for item in self:
+            if not item.seats:
+                item.taken_seats = 0
+            else:
+                item.taken_seats = 100 * len(item.attendee_ids) / item.seats
